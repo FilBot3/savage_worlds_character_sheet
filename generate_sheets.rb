546aa342dc0e1cdb5ef88836b19ff5b8ae461f87
Phillip dudley <Predatorian3@gmail.com>
2020-01-23 22:31:20 -0600

    partially ready. need to fix character ata and mobile template

--- generate_sheets.rb
+++ generate_sheets.rb
@@ -11,11 +11,19 @@ character_data_file = ARGV[0]
 
 config = YAML.safe_load(File.read(character_data_file))
 
-template = ERB.new(File.read('mobile_template/savage_worlds_mobile.html.erb'), nil, "%")
+mobile_template = ERB.new(File.read('mobile_template/savage_worlds_mobile.html.erb'), nil, "%")
 #template.run
 
 File.open("character_sheets/#{config['character_name']}_mobile_character_sheet.html", 'w') do |file|
-  file.write(template.result(binding))
+  file.write(mobile_template.result(binding))
 end
 
 puts "=> Generated character_sheets/#{config['character_name']}_mobile_character_sheet.html"
+
+desktop_template = ERB.new(File.read('desktop_template/savage_worlds_desktop.html.erb'), nil, "%")
+
+File.open("character_sheets/#{config['character_name']}_desktop_character_sheet.html", 'w') do |file|
+  file.write(desktop_template.result(binding))
+end
+
+puts "=> Generated character_sheets/#{config['character_name']}_desktop_character_sheet.html"
